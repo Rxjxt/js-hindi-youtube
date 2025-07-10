7c2f29806740206c1ff5709e738c89afc13e4283
Rxjxt <rajatsinghhhh503@gmail.com>
2025-07-10 13:59:22 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert("3 + 3") // we are using nodejs, not browser
+
+console.log(3
+    +3)  //could readibility should be high
+
+console.log("Hitesh")
+
+
+
+let name = "hitesh"
+let age  = 18
+let isLoggedIn = false
+
+//datatypes
+//number => 2 to power 53
+// bigint
+// string ->single ya double quote
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol -> uniqueness ke liye use hota hai
+
+
+//object 
+
+console.log(typeof age);
+console.log(typeof null)// to yeh object aayega output mei and undefined ka outpt undefined hi hoga
